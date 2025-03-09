253eea8b7258bf11172d66b582371e3f13b1338f
Harsh Kumar <97897760+harshverma4028@users.noreply.github.com>
2025-03-09 19:05:09 +0530

    update 2.0

---
+++ 1.LLMs/1_llm_demo.py
@@ -0,0 +1,40 @@
+from langchain_openai import OpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm = OpenAI(model='gpt-3.5-turbo-instruct')
+
+result = llm.invoke("what is the capital of india")
+print(result)
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from ai import streamText
+# from ai_sdk.openai import createOpenAI
+# from instructions import CHAT_INSTRUCTIONS
+
+# # Set token, endpoint, and model name (free model setup)
+# token = Kaat raha hu push ke liye . env me daal  de raha hu
+# endpoint = "https://models.inference.ai.azure.com"
+# modelName = "gpt-4o"
+
+# # Function to invoke the model
+# def invoke_model(prompt):
+#     llm = createOpenAI(api_key=token, base_url=endpoint, model=modelName)
+#     response = llm.invoke(prompt)
+#     return response
+
+# # Example usage
+# result = invoke_model("What is the capital of India?")
+# print(result)
